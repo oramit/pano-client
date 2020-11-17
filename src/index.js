69fa388c4a7f6ca6d671473d71d2d5a59e32a352
Or Amit <oramit1985@gmail.com>
2020-11-17 12:25:13 +0200

    clear timer.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const { numbers } = require('./data.js');
 const serverConnector = require('./serverConnector.js');
 const { SERVER_CALC_CAPACITY} = require('./common/consts.js');
 
+const totalNumbersToCalc = numbers.length;
 let requestsToPoll = [];
 let results = {};
 
@@ -19,16 +20,23 @@ async function startCalculate() {
 
     requestsToPoll = requestsIdsWithInput.map(reqWithNum => reqWithNum.reqId);
 
-    setInterval(() => {
+    let timer = setInterval(() => {
         Promise.all(requestsToPoll.map(reqId => {
             serverConnector.queryCalcResult(reqId).then(result => {
                 results[reqId].result = result;
-                requestsToPoll = requestsToPoll.filter(req => req !== reqId);
-                let nextNumberToCalc = numbers.pop();
-                serverConnector.queryCalc(nextNumberToCalc).then(reqId => {
-                    requestsToPoll.push(reqId);
-                    results[reqId] = { input: nextNumberToCalc };
-                });
+                requestsToPoll = requestsToPoll.length > 0 ? requestsToPoll.filter(req => req !== reqId) : [];
+                
+                if(numbers.length > 0) {
+                    let nextNumberToCalc = numbers.pop();
+                    serverConnector.queryCalc(nextNumberToCalc).then(reqId => {
+                        requestsToPoll.push(reqId);
+                        results[reqId] = { input: nextNumberToCalc };
+                    });
+                }
+                // If all numbers were calculated..
+                if(Object.values(results).filter(resEntry => resEntry && resEntry.result).length === totalNumbersToCalc) {
+                    clearInterval(timer);
+                }
             });    
         }));
     }, 1000);
