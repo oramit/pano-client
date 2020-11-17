6287318d5b1db5a0cf1979e5e30c88cf37757a63
Or Amit <oramit1985@gmail.com>
2020-11-17 19:44:23 +0200

    delete tests.

--- src/server/utils.test.js
+++
@@ -1,8 +0,0 @@
-jest.mock('./connector');
-
-const utils = require('./utils');
-
-test('new api test', async () => {
-    const result = await utils.queryCalc(1);
-    expect(result).toStrictEqual({request_id: '26c6a2bd-ddbc-49e8-b9fb'});
-});
